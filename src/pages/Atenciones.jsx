8ce0222f15c280da0aae1efa73022e2d11102410
Fernando Joost <fjoostr@gmail.com>
2023-03-22 10:24:56 -0300

    add show/hide information and button actions

--- src/pages/Atenciones.jsx
+++ src/pages/Atenciones.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/Atenciones.css";
 
 const Atenciones = () => {
-  const [expandedRows, setExpandedRows] = useState([]);
+  const [expandedRow, setExpandedRow] = useState([]);
 
   const Atenciones = [
     {
@@ -12,10 +12,10 @@ const Atenciones = () => {
       categoria: "Category 1",
       profesional: "Dr. Juan Perez",
       meap: true,
-      // edad_paciente,
-      // motivo,
-      // anamnesis,
-      // exploracion,
+      edad_paciente: "20",
+      motivo: "Motivo de la consulta",
+      anamnesis: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      exploracion: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
       id: 2,
@@ -24,10 +24,10 @@ const Atenciones = () => {
       categoria: "Category 2",
       profesional: "Dra. Maria Rosas",
       meap: true,
-      // edad_paciente,
-      // motivo,
-      // anamnesis,
-      // exploracion,
+      edad_paciente: "11",
+      motivo: "Motivo de la consulta",
+      anamnesis: "lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      exploracion: "lorem ipsum dolor sit amet consectetur adipisicing elit. ",
     },
     {
       id: 3,
@@ -36,24 +36,19 @@ const Atenciones = () => {
       categoria: "Category 3",
       profesional: "Dr. Juan Perez",
       meap: true,
-      // edad_paciente,
-      // motivo,
-      // anamnesis,
-      // exploracion,
+      edad_paciente: "22",
+      motivo: "Motivo de la consulta",
+      anamnesis: "lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      exploracion: "lorem ipsum dolor sit amet consectetur adipisicing elit. ",
     },
   ];
 
   const handleExpandRow = (id) => {
-    const currentIndex = expandedRows.indexOf(id);
-    const newExpandedRows = [...expandedRows];
-
-    if (currentIndex === -1) {
-      newExpandedRows.push(id);
+    if (expandedRow === id) {
+      setExpandedRow(null);
     } else {
-      newExpandedRows.splice(currentIndex, 1);
+      setExpandedRow(id);
     }
-
-    setExpandedRows(newExpandedRows);
   };
 
   return (
@@ -75,16 +70,35 @@ const Atenciones = () => {
                 <td>{atencion.fecha}</td>
                 <td>{atencion.servicio}</td>
                 <td>{atencion.categoria}</td>
+                <td>{atencion.profesional}</td>
                 <td>
                   <button onClick={() => handleExpandRow(atencion.id)}>
-                    {expandedRows.includes(atencion.id) ? "Hide" : "Show"}
+                    {expandedRow === atencion.id ? "Hide" : "Show"}
                   </button>
                 </td>
               </tr>
-              {expandedRows.includes(atencion.id) && (
-                <tr>
-                  <td colSpan="3">{atencion.servicio}</td>
-                </tr>
+              {expandedRow === atencion.id && (
+                <>
+                  <tr>
+                    <td colSpan="3">{atencion.meap ? "Meap" : " "}</td>
+                    <td colSpan="3">
+                      Edad del paciente: {atencion.edad_paciente}
+                    </td>
+                    <td colSpan="3">Médico: {atencion.profesional}</td>
+                  </tr>
+                  <tr>
+                    <td>Motivo</td>
+                    <td>{atencion.motivo}</td>
+                  </tr>
+                  <tr>
+                    <td>Anamnesis</td>
+                    <td>{atencion.anamnesis}</td>
+                  </tr>
+                  <tr>
+                    <td>Exploracion</td>
+                    <td>{atencion.exploracion}</td>
+                  </tr>
+                </>
               )}
             </React.Fragment>
           ))}
